bd11006c4b5eda4ba583910eeaee042756076f0a
TraV5 <v.nazarkin16@gmail.com>
2023-05-16 01:45:31 +0300

    Добавлено здоровье. Добавлен урон от врага. Добавлена анимация неуязвимости. Прикреплен Build 0.2

--- Journey to Elbrus/player/Enemy.gd
+++ Journey to Elbrus/player/Enemy.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
 
+@export var damage := 1
 var gravity = 300
+
 
 @onready var animated_sprite := $AnimatedSprite2D
 
@@ -15,3 +17,6 @@ func _play_animation(animation: String):
 func die():
 	_play_animation("death")
 	$CollisionShape2D.set_deferred("disabled", true)
+	
+func attack(player: CharacterBody2D):
+	player.take_damage(damage, global_position.direction_to(player.global_position))
--- Journey to Elbrus/player/Player.gd
+++ Journey to Elbrus/player/Player.gd
@@ -2,13 +2,18 @@ extends CharacterBody2D
 
 signal hp_changed
 
+const IMMORTAL_TIME = 2
+
 var run_speed = 150
 const jump_speed = -200
 var gravity = 300
 var hp = 8
+var immortal_time = 0.0
 
 
 @onready var animated_sprite := $AnimatedSprite2D
+@onready var ray_cast_down := $RayCastDown
+@onready var animation_player = $AnimationPlayer
 
 func _ready() -> void:
 	call_deferred("change_hp", 0)
@@ -29,13 +34,26 @@ func get_input():
 	if left:
 		velocity.x -= run_speed
 		animated_sprite.flip_h = true
-
+	if land:
+		velocity.y += 15
+	if not is_on_floor() and velocity.y > 0 and ray_cast_down.is_colliding():
+		var enemy = ray_cast_down.get_collider()
+		hit_enemy(enemy)
+		
 func _physics_process(delta: float):
+	
 	velocity.y += gravity * delta
 	
 	get_input()
+	
 	_play_move_animation(velocity)
 	move_and_slide()
+	
+	if immortal_time >= 0:
+		immortal_time -= delta
+		animation_player.play("Immortal")
+	else:
+		animation_player.stop()
 	
 func _play_move_animation(velocity: Vector2):
 	if velocity.x != 0 and is_on_floor():
@@ -51,16 +69,14 @@ func _play_animation(animation: String):
 	if animated_sprite.animation != animation:
 		animated_sprite.play(animation)
 
-func on_enemy_contact(enemy: Node2D):
-	hit_enemy(enemy)
-
 func hit_enemy(enemy: Node2D):
 	if enemy.has_method("die"):
 		enemy.die()
 	velocity.y = jump_speed / 2.0
 
-func take_damage():
-	change_hp(-1)
+func take_damage(damage: int, attack_direction: Vector2):
+	change_hp(-damage)
+	immortal_time = IMMORTAL_TIME
 	
 func change_hp(diff: int):
 	hp += diff
